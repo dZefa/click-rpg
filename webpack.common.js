5e60d582bcc1191138abf676c72e683965df90b2
Daniel Chong <danchong625@gmail.com>
2018-06-07 16:51:44 -0700

    [ops] initializes webpack compiler with dev and prod

---
+++ webpack.common.js
@@ -0,0 +1,62 @@
+const webpack = require('webpack');
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+const ExtractCSSPlugin = require('mini-css-extract-plugin');
+
+const APP_DIR = path.resolve(__dirname, './src/index.tsx');
+const BUILD_DIR = path.resolve(__dirname, './build');
+const TEMPLATE_DIR = path.resolve(__dirname, './src/template.index.html');
+
+module.exports = {
+  entry: {
+    app: APP_DIR,
+  },
+  output: {
+    path: BUILD_DIR,
+    filename: '[name].bundle.js',
+  },
+  resolve: {
+    extensions: [
+      "ts", "tsx", "js"
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.(png|jpg|jpeg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'assets/[hash].[ext]',
+            },
+          }
+        ]
+      },
+      {
+        test: /scss$/,
+        use: [
+          ExtractCSSPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new CleanPlugin(['build']),
+    new ExtractCSSPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new HTMLPlugin({
+      template: TEMPLATE_DIR,
+      inject: true,
+    })
+  ]
+}
