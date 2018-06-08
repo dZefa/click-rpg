1d293eb7b6f41d3d818d0d1d527eaf339821d516
Daniel Chong <danchong625@gmail.com>
2018-06-07 18:19:59 -0700

    [ops] removes tsconfig paths plugin

--- webpack.common.js
+++ webpack.common.js
@@ -3,12 +3,10 @@ const path = require('path');
 const HTMLPlugin = require('html-webpack-plugin');
 const CleanPlugin = require('clean-webpack-plugin');
 const ExtractCSSPlugin = require('mini-css-extract-plugin');
-const TSconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 const APP_DIR = path.resolve(__dirname, './src/index.tsx');
 const BUILD_DIR = path.resolve(__dirname, './build');
 const TEMPLATE_DIR = path.resolve(__dirname, './src/template.index.html');
-const TSconfig_DIR = path.resolve(__dirname, './tsconfig.json');
 
 module.exports = {
   entry: {
@@ -20,7 +18,7 @@ module.exports = {
   },
   resolve: {
     extensions: [
-      "ts", "tsx", "js"
+      ".ts", ".tsx", ".js", ".json"
     ],
   },
   module: {
@@ -59,9 +57,6 @@ module.exports = {
     new HTMLPlugin({
       template: TEMPLATE_DIR,
       inject: true,
-    }),
-    new TSconfigPathsPlugin({
-      configFile: TSconfig_DIR,
     })
   ]
-}
+};
