19f66b5da04d4158a896bbc36587e59b94d83f60
Daniel Chong <danchong625@gmail.com>
2018-06-07 18:26:21 -0700

    [srvr] adds serving of static files

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import Express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import morgan from 'morgan';
+import path from 'path';
 
 const app = Express();
 
@@ -12,5 +13,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('short'));
 }
+
+app.use(Express.static(path.resolve(__dirname, process.env.BUILD_PATH)));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, `${process.env.BUILD_PATH}/index.html`));
+});
 
 export { app };
