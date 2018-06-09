1fa5aa696782a6e0347ade148e65ac0a06a5171b
Daniel Chong <danchong625@gmail.com>
2018-06-09 13:44:36 -0700

    [gm] initializes game config

---
+++ src/game/game.ts
@@ -0,0 +1,35 @@
+import 'phaser';
+
+const config: GameConfig = {
+  title: "Click RPG",
+  url: "https://github.com/dzefa/click-rpg",
+  version: "0.1",
+  width: 600,
+  height: 800,
+  zoom: 1,
+  type: Phaser.AUTO,
+  parent: "game",
+  scene: [],
+  input: {
+    keyboard: true,
+    mouse: true,
+    touch: true,
+    gamepad: false,
+  },
+  physics: {
+    default: "arcade",
+    gravity: {
+      y: 0,
+    },
+    debug: false,
+  },
+  backgroundColor: "#FFFFFF",
+  "render.pixelArt": true,
+  "render.antialias": false,
+};
+
+export class Game extends Phaser.Game {
+  constructor(config: GameConfig) {
+    super(config);
+  }
+}
