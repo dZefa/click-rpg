b1c999ab0a4984e77524f708c584ef3770f8a65e
Daniel Chong <danchong625@gmail.com>
2018-06-10 01:47:44 -0700

    [style] initializes basic styling for game view

--- src/components/game/gameView.tsx
+++ src/components/game/gameView.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 
+import './gameView.scss';
+
 export class GameView extends React.Component<{}> {
   render() {
     return (
-      <div>
+      <div id="game-view">
         <div id="game"></div>
       </div>
     )
